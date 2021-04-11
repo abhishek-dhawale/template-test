5e892da0644e2a9fa664a732e19956e45f83d291
Abhishek Dhawale <dhawale.abhishek@gmail.com>
2021-04-11 12:48:22 +0530

    quote generator

---
+++ script.js
@@ -0,0 +1,78 @@
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show Loader
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden=true;
+}
+
+// Hide Loading
+
+function complete()
+ {
+   quoteContainer.hidden = false;
+   loader.hidden = true;
+ }
+// Show New Quote
+
+function newQuote() {
+  loading();
+  // Pick a random quote from apiQuote array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check id Author field is blank and replace it with
+  if(!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check Quote length to determine styling
+
+  if(quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  //Set the quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+//  Get quotes from API
+
+async function getQuotes() {
+  loading();
+  const apiUrl ='https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch(error){
+    alert('Error');
+    //  Catch error Here
+  }
+}
+
+// Tweet Quote
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+
+getQuotes();
